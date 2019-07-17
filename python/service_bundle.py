a5d7c59e68798e44bc137fe2e8ca9850a5d70b68
Uladzimir Krautsou <ukrautsou@mesosphere.com>
2019-07-17 20:23:58 +0300

    DCOS-48421 [Diagnostics] Allow sorting of tasks directories fetched by service diagnostic bundle. #exported var

--- python/service_bundle.py
+++ python/service_bundle.py
@@ -50,9 +50,10 @@ class ServiceBundle(Bundle):
         tasks_by_agent_id = dict(groupby("slave_id", all_tasks))
 
         for agent_id, tasks in tasks_by_agent_id.items():
+            debug_agent_files = agent.debug_agent_files(agent_id)
             for task in tasks:
                 task_executor_sandbox_path = sdk_diag._find_matching_executor_path(
-                    agent.debug_agent_files(agent_id), sdk_diag._TaskEntry(task)
+                    debug_agent_files, sdk_diag._TaskEntry(task)
                 )
                 if task_executor_sandbox_path:
                     agent.download_task_files(
