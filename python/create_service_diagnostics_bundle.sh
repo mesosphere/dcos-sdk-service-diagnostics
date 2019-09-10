f052e963c9239cbe5a6505f4b173fb8d0d5b0b4c
Uladzimir Krautsou <ukrautsou@mesosphere.com>
2019-09-10 16:47:41 +0300

    DCOS-58484 introduced TTY_OPTS env vars

--- python/create_service_diagnostics_bundle.sh
+++ python/create_service_diagnostics_bundle.sh
@@ -11,6 +11,7 @@ readonly BUNDLES_DIRECTORY="service-diagnostic-bundles"
 readonly DOCKER_IMAGE="mesosphere/dcos-sdk-service-diagnostics:${VERSION}"
 readonly SCRIPT_NAME="create_service_diagnostics_bundle.py"
 readonly HOST_DCOS_CLI_DIRECTORY="${DCOS_DIR:-${HOME}/.dcos}"
+readonly TTY_OPTS="${TTY_OPTS:=-it}"
 
 readonly CONTAINER_BUNDLES_DIRECTORY="/${BUNDLES_DIRECTORY}"
 readonly CONTAINER_DCOS_CLI_DIRECTORY_RO="/dcos-cli-directory"
@@ -66,7 +67,7 @@ mkdir -p "${BUNDLES_DIRECTORY}"
 function container_run () {
   local -r command="${*:-}"
   docker run \
-         -t \
+         "${TTY_OPTS}" \
          --rm \
          -v "$(pwd)/${BUNDLES_DIRECTORY}:${CONTAINER_BUNDLES_DIRECTORY}" \
          -v "${HOST_DCOS_CLI_DIRECTORY}:${CONTAINER_DCOS_CLI_DIRECTORY_RO}":ro \
@@ -109,4 +110,4 @@ container_run "rm -rf ${CONTAINER_DCOS_CLI_DIRECTORY} && mkdir ${CONTAINER_DCOS_
                  -exec cp --parents \{\} ${CONTAINER_DCOS_CLI_DIRECTORY} \;
                cd /
                dcos plugin add /tmp/dcos-core-cli-${DCOS_CLUSTER_MAJOR_MINOR_VERSION}.zip
-               ${CONTAINER_SCRIPT_PATH} ${*} --bundles-directory ${CONTAINER_BUNDLES_DIRECTORY} --yes"
+               ${CONTAINER_SCRIPT_PATH} ${*} --bundles-directory ${CONTAINER_BUNDLES_DIRECTORY}"
