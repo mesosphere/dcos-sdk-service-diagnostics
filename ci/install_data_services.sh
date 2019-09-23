f75a609b1060e4fefb8532f3fd578a748fb3f256
Uladzimir Krautsou <ukrautsou@mesosphere.com>
2019-09-23 23:24:43 +0300

    DCOS-59137 [Diagnostics] Introduce the dcos-sdk-service-diagnostics component version managment.

--- ci/install_data_services.sh
+++ ci/install_data_services.sh
@@ -2,11 +2,11 @@
 set -eu -o pipefail
 
 readonly HOST_DCOS_CLI_DIRECTORY="${DCOS_DIR:-${HOME}/.dcos}"
-#ToDo: Once the image version will be manageble dont forget replace it here.
+readonly BUILD_IMAGE_VERSION=${BUILD_IMAGE_VERSION:-$(<VERSION)}
 docker run \
   -t \
   --rm \
   -v "${HOST_DCOS_CLI_DIRECTORY}:/dcos-cli-directory":ro \
   -v "$(pwd)/ci:/ci" \
-  "mesosphere/dcos-sdk-service-diagnostics:v0.4.0" \
+  "mesosphere/dcos-sdk-service-diagnostics:${BUILD_IMAGE_VERSION}" \
   /ci/install_data_services.py "$@"
