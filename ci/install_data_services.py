3ce1ae57975685ff95a7bcdb2e515d7669927d8e
Andriy Yatskovets <mediapills@gmail.com>
2019-10-22 18:42:07 +0300

    redesign install_data_services

--- ci/install_data_services.py
+++ ci/install_data_services.py
@@ -5,37 +5,59 @@ import sys
 import sdk_install, sdk_cmd
 import logging
 
+from concurrent import futures
+
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
+MAX_WORKERS=10
 
-def main() -> int:
+
+def install_package(service_name):
     """
-    This function checking authentication and setup DC/OS service
+    :param service_name: DC/OS service name
+    :type service_name: str
+    """
+    log.info("Starting %s installation.", service_name)
+    package_name = service_name
+
+    sdk_install.install(
+        package_name,
+        service_name,
+        0,
+        package_version=sdk_install.PackageVersion.LATEST_UNIVERSE
+    )
+
+
+def main(args) -> int:
+    """
+    This function setup DC/OS service
+
+    :param args: CLI arguments
+    :type args: list
 
     :return: Execution status
-    :rtype: int
+    :rtype: list
     """
-    if len(sys.argv) != 2:
-        log.error("Script %s support only one argument", sys.argv[0])
-        return 1
+    log.info("Starting %s installation.", ' '.join(args))
 
-    service_name = sys.argv[1]
-    log.info("Starting %s installation.", service_name)
+    result = 0
+    # Statement to ensure threads are cleaned up promptly
+    with futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as pool:
+        threads = {pool.submit(install_package, service) for service in args}
+        for thread in futures.as_completed(threads):
+            try:
+                thread.result()
+            except Exception as e:
+                log.error(str(e))
+                result = 1
 
-    try:
-        sdk_install.install(
-            service_name,
-            service_name,
-            0,
-            package_version=sdk_install.PackageVersion.LATEST_UNIVERSE
-        )
-    except Exception as e:
-        print(str(e))
-        return 1
-
-    return 0
+    return result
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    if len(sys.argv) < 2:
+        log.error("At least one argument required.")
+        sys.exit(1)
+
+    sys.exit(main(sys.argv[1:]))
--- ci/install_data_services.sh
+++ ci/install_data_services.sh
@@ -11,48 +11,12 @@ readonly HOST_DCOS_CLI_DIRECTORY="${DCOS_DIR:-${HOME}/.dcos}"
 readonly SCRIPT_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 readonly VERSION=$("${SCRIPT_DIRECTORY}"/../python/create_service_diagnostics_bundle.sh -v | tail -1)
 
-# ###
-# 2. section: Define functions.
-# ###
-function install_service() {
-  echo "Run container for data service installation: $@"
+echo "Run container for data service installation: $@"
 
-  docker run \
-    -t \
-    --rm \
-    -v "${HOST_DCOS_CLI_DIRECTORY}:/dcos-cli-directory":ro \
-    -v "${SCRIPT_DIRECTORY}:/ci" \
-    "mesosphere/dcos-sdk-service-diagnostics:${VERSION}" \
-    /ci/install_data_services.py "$@"
-}
-
-function install_services() {
-  echo "Install DC/OS services: $@"
-
-  declare -a PIDS
-
-  # Run installation for all services.
-  for service in "$@"; do
-    install_service ${service} &
-
-    # store PID of process
-    PIDS[$!]=${service}
-  done
-
-  # Wait for all child processes to finish and handle error codes.
-  for PID in "${!PIDS[@]}"; do
-    wait "$PID" || (
-      echo "Error occurred during service installation: ${PIDS[PID]}" >&2
-      exit 1
-    )
-  done
-}
-
-# ###
-# 3. section: Main script execution.
-# ###
-if [ "${#}" -eq 1 ] && [ "$1" == "--debug" ]; then
-  eval "${SCRIPT_DIRECTORY}/$(basename $0) cassandra confluent-kafka confluent-zookeeper datastax-dse datastax-ops elastic hdfs kafka-zookeeper dcos-monitoring"
-else
-  install_services $@
-fi
+docker run \
+  -t \
+  --rm \
+  -v "${HOST_DCOS_CLI_DIRECTORY}:/dcos-cli-directory":ro \
+  -v "${SCRIPT_DIRECTORY}:/ci" \
+  "mesosphere/dcos-sdk-service-diagnostics:${VERSION}" \
+  /ci/install_data_services.py "$@"
